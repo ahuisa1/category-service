731de9cee9e71fb0f4b869c6fa8ca6fa4b5be04d
ahuisa <ahuisa@smv.gob.pe>
2026-07-15 09:45:56 -0500

    add pipeline infra + bicep

--- obj/Debug/net8.0/CategoryService.AssemblyInfo.cs
+++ obj/Debug/net8.0/CategoryService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5607f4efcc72a680849f6d1d74d209f2782c48c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
